0e1045c9d4da8764a445e2e69e41f7dcd28a146b
cochransean <cochransean@gmail.com>
2016-06-20 10:57:01 -0400

    split contracts and made changes to NLTK script to reflect new output data structure

--- NLTK Test.py
+++ NLTK Test.py
@@ -2,13 +2,15 @@ import csv
 from pycorenlp import StanfordCoreNLP
 import time
 import re
+import ast
 
 # TODO fix Sep 11, 2006 to Nov 15, 2006 and November 29, 2006 to Dec 4, 2006 data (not separated)
 # TODO try this regex to find seperators between the large blocks of text \([a-zA-z0-9-]{6,}[0-9]{3,}\)
 # TODO find items with more than one match maybe with
 # TODO (\([a-zA-z0-9-]{8,}[0-9]{3,}\)).{300,}(\([a-zA-z0-9-]{8,}[0-9]{3,}\))
 # setup variables and server
-contracts = []
+dates = []
+results = []
 nlp = StanfordCoreNLP('http://localhost:9000')
 
 
@@ -16,10 +18,10 @@ def ner_tag_text():
 
     # load data
     try:
-        with open('consolidated_contracts.csv', 'r') as csvfile:
+        with open('consolidated_contracts_split.csv', 'r') as csvfile:
             contract_reader = csv.DictReader(csvfile)
             for row in contract_reader:
-                contracts.append({'date': row['date'], 'announcement': row['announcement']})
+                dates.append({'date': row['date'], 'announcements': ast.literal_eval(row['announcement'])})
     except IOError:
         print("Error loading data.")
     else:
@@ -33,69 +35,65 @@ def ner_tag_text():
     }
 
     # loop through each press release
-    for contract in contracts:
-        # get rid of troublesome unicode characters
-        contract['announcement'] = re.sub("[\x92\x93\x94]", "", contract['announcement'])
-        contract['announcement'] = contract['announcement'].replace("’", "'")
-        contract['announcement'] = contract['announcement'].replace("“", '"')
-        contract['announcement'] = contract['announcement'].replace("”", '"')
-        contract['announcement'] = contract['announcement'].replace("–", '-')
-        contract['announcement'] = contract['announcement'].replace("—", '-')
-        contract['announcement'] = contract['announcement'].replace("‑", '-')
+    for date in dates:
+        for contract in date['announcements']:
+            # get rid of troublesome unicode characters
+            contract = re.sub("[\x92\x93\x94]", "", contract)
+            contract = contract.replace("’", "'")
+            contract = contract.replace("“", '"')
+            contract = contract.replace("”", '"')
+            contract = contract.replace("–", '-')
+            contract = contract.replace("—", '-')
+            contract = contract.replace("‑", '-')
 
-        tagged_contract = []
-        annotated_text = None
-        # get text of release
-        successful = False
-        number_of_tries = 0
-        while not successful and number_of_tries < 10:
-            text = contract['announcement']
-            print(text)
-            annotated_text = nlp.annotate(text, properties={
-                'annotators': 'ner',
-                'outputFormat': 'json'
-            })
-            # check for errors and repeat if needed (server sometimes can't handle requests)
-            if type(annotated_text) == str:
-                print('Error with server: ' + annotated_text)
-                # pause in case server is overloaded
-                time.sleep(5)
-                number_of_tries += 1
-                if number_of_tries >= 10:
-                    print('10 failed tries with server. Saving current results and exiting.')
-                    return
-            else:
-                successful = True
+            tagged_contract = []
+            annotated_text = None
+            # get text of release
+            successful = False
+            number_of_tries = 0
+            while not successful and number_of_tries < 10:
+                annotated_text = nlp.annotate(contract, properties={
+                    'annotators': 'ner',
+                    'outputFormat': 'json'
+                })
+                # check for errors and repeat if needed (server sometimes can't handle requests)
+                if type(annotated_text) == str:
+                    print('Error with server: ' + annotated_text)
+                    # pause in case server is overloaded
+                    time.sleep(5)
+                    number_of_tries += 1
+                    if number_of_tries >= 10:
+                        print('10 failed tries with server. Saving current results and exiting.')
+                        return
+                else:
+                    successful = True
 
-        # loop through sentences
-        for sentence in annotated_text['sentences']:
             # create dict to store output
-            entry = {}
+            entry = dict()
+            entry.update({'press_release': contract, 'date': date['date']})
             # track current list index for each output type
             output_index = {}
             for output in classifiers_to_output.values():
                 entry[output] = []
                 output_index[output] = 0
-            # loop through words and punctuation
-            for current_token, next_token in zip(sentence['tokens'], sentence['tokens'][1:]):
-                for classifier, output in classifiers_to_output.items():
-                    # make sure current word and next match classifier (to separate identical matches in same sentence)
-                    if current_token['ner'] == classifier:
-                        # add space to list if it isn't there
-                        if len(entry[output]) < output_index[output] + 1:
-                            entry[output].append('')
-                        # include any spaces between words
-                        entry[output][output_index[output]] += current_token['originalText'] + current_token['after']
-                        if next_token['ner'] != classifier:
-                            output_index[output] += 1
-            tagged_contract.append(entry)
-        # add pertinent data to contracts list
-        contract['organization'] = tagged_contract[0]['organization']
-        contract['value'] = tagged_contract[0]['value']
-        contract['location'] = tagged_contract[0]['location']
-        contract['contracting_activity'] = tagged_contract[-1]['organization']
-        contract['contracting_activity_location'] = tagged_contract[-1]['location']
-        print(contract)
+
+            # loop through sentences
+            for sentence in annotated_text['sentences']:
+                # loop through words and punctuation
+                for current_token, next_token in zip(sentence['tokens'], sentence['tokens'][1:]):
+                    for classifier, output in classifiers_to_output.items():
+                        # make sure current word and next match classifier (to separate identical matches in sentence)
+                        if current_token['ner'] == classifier:
+                            # add space to list if it isn't there
+                            if len(entry[output]) < output_index[output] + 1:
+                                entry[output].append('')
+                            # include any spaces between words
+                            entry[output][output_index[output]] += current_token['originalText'] + current_token['after']
+                            if next_token['ner'] != classifier:
+                                output_index[output] += 1
+                tagged_contract.append(entry)
+            # add pertinent data to out
+            results.append(entry)
 
 
 def save_results():
@@ -106,13 +104,13 @@ def save_results():
                           'announcement', 'contracting_activity_location']
             contract_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             contract_writer.writeheader()
-            for contract in contracts:
+            for result in results:
                 unique_id = 0
-                contract_writer.writerow({'date': contract['date'], 'organization': contract['organization'],
-                                          'value': contract['value'], 'location': contract['location'],
-                                          'contracting_activity': contract['contracting_activity'],
-                                          'contracting_activity_location': contract['contracting_activity_location'],
-                                          'announcement': contract['announcement'], 'id': unique_id})
+                contract_writer.writerow({'date': result['date'], 'organization': result['organization'],
+                                          'value': result['value'], 'location': result['location'],
+                                          'contracting_activity': result['contracting_activity'],
+                                          'contracting_activity_location': result['contracting_activity_location'],
+                                          'announcement': result['announcement'], 'id': unique_id})
                 unique_id += unique_id
     except IOError:
         print("Error exporting data.")
